5fc41afe43c178d76d45f85be2593d17503a74c4
Mya <136588892+MariaAksamit@users.noreply.github.com>
2024-04-22 10:53:45 +0200

    Add files via upload
    Juli - +Add activity

--- client/src/bricks/SubjectDetail.js
+++ client/src/bricks/SubjectDetail.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect, useContext } from "react";
-import { Button, Navbar, Table, Tab, Tabs } from 'react-bootstrap';
-import { useNavigate } from "react-router-dom"
+import { Button, Navbar, Table, Tab, Tabs, Form } from 'react-bootstrap';
+import { useNavigate } from "react-router-dom";
 import UserContext from "../Provider";
-
+import AddActivity from "./AddActivity";
 import ActivityDetail from "./ActivityDetail";
 
 function SubjectDetail({ subjDetail, subjectTermL, activityL }) {
@@ -10,6 +10,29 @@ function SubjectDetail({ subjDetail, subjectTermL, activityL }) {
   const { users } = useContext(UserContext);
   const [selectedActivity, setSelectedActivity] = useState(null);
   const [selectedSubjectTerm, setSelectedSubjectTerm] = useState(null);
+  const [showAddActivityModal, setShowAddActivityModal] = useState(false);
+  const [deadline, setDeadline] = useState(""); // State variable to store the deadline
+
+  // Function to handle opening the modal to add an activity
+  const handleOpenAddActivityModal = () => {
+    setShowAddActivityModal(true);
+  };
+
+  // Function to handle closing the modal to add an activity
+  const handleCloseAddActivityModal = () => {
+    setShowAddActivityModal(false);
+  };
+
+  // Function to handle adding an activity
+  const handleAddActivity = (activityData) => {
+    // Update the activity data with the deadline
+    const activityWithDeadline = { ...activityData, deadline };
+    // Implement logic to add the activity
+    console.log("Adding activity with deadline:", activityWithDeadline);
+    // For example, you can update the activity list with the new activity
+    // Update the activity list using setActivityL([...activityL, activityWithDeadline]);
+    setShowAddActivityModal(false); // Close the modal after adding the activity
+  };
 
   // Funkcia na získanie subjTerms predmetu
   const getSubjectTerms = () => {
@@ -57,165 +80,159 @@ function SubjectDetail({ subjDetail, subjectTermL, activityL }) {
     }
   }, [subjDetail]);
 
-return (
-<>
-  <div className="formDetail">
-    <Navbar collapseOnSelect expand="sm" bg="light">
-      <div className="container-fluid">
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse style={{ justifyContent: "flex-end" }}>
-        <div className="formDetailButton">
-          <Button variant="primary" size="sm">
-            + SubjectTerm
-          </Button>
-          <Button variant="primary" size="sm">
-            + Activity
-          </Button>
-          <Button variant="primary" size="sm">
-            Enroll/Remove
-          </Button>
-          <Button variant="success" size="sm">
-            Update
-          </Button>
-          <Button variant="outline-secondary"  size="sm" onClick={handleBack}>
-            Back
-          </Button>
-        </div>  
-        </Navbar.Collapse> 
+  
+  return (
+    <>
+      <div className="formDetail">
+        <Navbar collapseOnSelect expand="sm" bg="light">
+          <div className="container-fluid">
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse style={{ justifyContent: "flex-end" }}>
+              <div className="formDetailButton">
+                <Button variant="primary" size="sm">
+                  + SubjectTerm
+                </Button>
+                <Button variant="primary" size="sm" onClick={handleOpenAddActivityModal}>
+                  + Activity
+                </Button>
+                <Button variant="primary" size="sm">
+                  Enroll/Remove
+                </Button>
+                <Button variant="success" size="sm">
+                  Update
+                </Button>
+                <Button variant="outline-secondary"  size="sm" onClick={handleBack}>
+                  Back
+                </Button>
+              </div>  
+            </Navbar.Collapse> 
+          </div>
+        </Navbar>
+        <br />
+        <div>
+          <b style={{ fontSize: '1.2em' }}>{subjDetail.name}</b>
+        </div>
+        <br />
+        <div>
+         <b> Credits: </b>{subjDetail.credits}
+        </div>
+        <div>
+          <b> Supervisor:  </b>{subjDetail.supervisor}
+        </div>
+        <div>
+          <b> Goal:  </b>{subjDetail.goal}
+        </div>
+        <div>
+          <b> Degree:  </b>{subjDetail.degree}
+        </div>
+        <div>
+          <b> Language:  </b>{subjDetail.language}
+        </div>
+        <div>
+          <b> School:  </b>{subjDetail.school}
+        </div>
+        <div>
+          <b> Description:  </b>{subjDetail.description}
+        </div>
+        <br />
+        <div>
+          <b>Subject Terms: </b>
+          <br />
+          <Tabs
+            id="subject-terms-tabs"
+            activeKey={selectedSubjectTerm ? selectedSubjectTerm.id : null}
+            onSelect={(key) => {
+              const term = subjectTerms.find(term => term.id === key);
+              if (term) handleSubjectTermClick(term);
+            }}
+          >
+            {subjectTerms.map((term) => (
+              <Tab
+                key={term.id}
+                eventKey={term.id}
+                title={term.semester}
+              />
+            ))}
+          </Tabs>
+        </div>
+        <br />
+        <Table striped bordered>
+          <thead>
+            <tr>
+              <th> Activity </th>
+              <th> Min Score </th>
+              <th> Max Score </th>
+              <th> Detail </th>
+            </tr>
+          </thead>
+          <tbody>
+            {getActivities().map((activity) => (
+              <tr key={activity.id}>
+                <td>{activity.name}</td>
+                <td>{activity.minScore}</td>
+                <td>{activity.maxScore}</td>
+                <td>
+                  <Button
+                    variant="outline-primary"
+                    size="sm"
+                    onClick={() => setSelectedActivity(activity)} // Nastaviť vybranú úlohu
+                  >
+                    {"<"} 
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+        <br />
+        <br />
+        <div> <b>Enrolled students: </b></div>
+        <Table striped bordered>
+          <thead>
+            <tr>
+              <th> Name </th>
+              <th> Surname </th>
+              <th> Grade </th>
+              <th> Detail </th>
+            </tr>
+          </thead>
+          <tbody>
+            {enrolledUsers.map((student) => (
+              <tr key={student.id}>
+                <td>{student.name}</td>
+                <td>{student.surname}</td>
+                <td>{student.grade}</td>
+                <td>
+                  <Button
+                    variant="outline-primary"
+                    size="sm"
+                  >
+                    {"<"} 
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+  
+        {selectedActivity && (
+          <ActivityDetail
+            activity={selectedActivity}
+            subjectTerm={selectedSubjectTerm}
+            onClose={() => setSelectedActivity(null)}
+          />
+        )}
       </div>
-    </Navbar>
-  <br />
-    <div>
-      <b style={{ fontSize: '1.2em' }}>{subjDetail.name}</b>
-    </div>
-  <br />
-    <div>
-     <b> Credits: </b>{subjDetail.credits}
-    </div>
-    <div>
-    <b> Supervisor:  </b>{subjDetail.supervisor}
-    </div>
-    <div>
-    <b> Goal:  </b>{subjDetail.goal}
-    </div>
-    <div>
-    <b> Degree:  </b>{subjDetail.degree}
-    </div>
-    <div>
-    <b> Language:  </b>{subjDetail.language}
-    </div>
-    <div>
-    <b> School:  </b>{subjDetail.school}
-    </div>
-    <div>
-    <b> Description:  </b>{subjDetail.description}
-    </div>
-  <br />
-  <div>
-    <b>Subject Terms: </b>
-  <br />
-    <Tabs
-      id="subject-terms-tabs"
-      activeKey={selectedSubjectTerm ? selectedSubjectTerm.id : null}
-      onSelect={(key) => {
-      const term = subjectTerms.find(term => term.id === key);
-        if (term) handleSubjectTermClick(term);
-      }}
-    >
-    {subjectTerms.map((term) => (
-      <Tab
-        key={term.id}
-        eventKey={term.id}
-        title={term.semester}
+  
+      {/* AddActivity modal */}
+      <AddActivity
+        show={showAddActivityModal}
+        handleClose={handleCloseAddActivityModal}
+        handleAddActivity={handleAddActivity}
       />
-    ))}
-      </Tabs>
-    </div>
- {/* Verzia s buttons
-  {/* <div>
-  <div style={{ display: 'flex' }}>
-    {subjectTerms.map((term) => (
-      <Button 
-        key={term.subjectTermId}
-        variant={term === selectedSubjectTerm ? "outline-primary" : "secondary-outline"}
-        onClick={() => handleSubjectTermClick(term)}
-        style={{ marginRight: '5px' }}
-      >
-        {term.semester}
-      </Button>
-    ))}
-  </div>
-  </div> */}
-<br />
-  <Table striped bordered>
-    <thead>
-      <tr>
-        <th> Activity </th>
-        <th> Min Score </th>
-        <th> Max Score </th>
-        <th> Detail </th>
-      </tr>
-    </thead>
-    <tbody>
-      {getActivities().map((activity) => (
-        <tr key={activity.id}>
-          <td>{activity.name}</td>
-          <td>{activity.minScore}</td>
-          <td>{activity.maxScore}</td>
-          <td>
-            <Button
-              variant="outline-primary"
-              size="sm"
-              onClick={() => setSelectedActivity(activity)} // Nastaviť vybranú úlohu
-            >
-              {"<"} 
-            </Button>
-          </td>
-        </tr>
-      ))}
-    </tbody>
-  </Table>
-<br />
-<br />
-<div> <b>Enrolled students: </b></div>
-<Table striped bordered>
-    <thead>
-      <tr>
-        <th> Name </th>
-        <th> Surname </th>
-        <th> Grade </th>
-        <th> Detail </th>
-      </tr>
-    </thead>
-    <tbody>
-      {enrolledUsers.map((student) => (
-        <tr key={student.id}>
-          <td>{student.name}</td>
-          <td>{student.surname}</td>
-          <td>{student.grade}</td>
-          <td>
-            <Button
-              variant="outline-primary"
-              size="sm"
-            >
-              {"<"} 
-            </Button>
-          </td>
-        </tr>
-      ))}
-    </tbody>
-  </Table>
-
-  {selectedActivity && (
-    <ActivityDetail
-      activity={selectedActivity}
-      subjectTerm={selectedSubjectTerm}
-      onClose={() => setSelectedActivity(null)}
-    />
-  )}
-</div>
     </>   
   );
+  
 }
 
 export default SubjectDetail;
