69f3fb45a98f53fdf844aacd2ff7df7a973833b6
IvaT <snufkins.hat@gmail.com>
2024-04-22 09:48:57 +0200

    Add dao and storage files

---
+++ server/dao/activity-dao.js
@@ -0,0 +1,17 @@
+"use strict";
+const fs = require("fs");
+const path = require("path");
+
+// load promise api for reading and writing file
+const rf = fs.promises.readFile;
+const wf = fs.promises.writeFile;
+
+// specify the default location of json file where the data should be stored
+const STORAGE_PATH = path.join(__dirname, "storage", "activities.json");
+
+class ActivityDao {
+
+
+}
+
+module.exports = ActivityDao;
