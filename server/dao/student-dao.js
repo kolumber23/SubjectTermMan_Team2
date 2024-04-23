12c5110812cd829204ab17d11309b2b62599643b
IvaT <snufkins.hat@gmail.com>
2024-04-23 10:19:56 +0200

    Add all methods except subjectTerm/enroll and remoteStudent and listStudents

--- server/dao/student-dao.js
+++ server/dao/student-dao.js
@@ -10,8 +10,42 @@ const wf = fs.promises.writeFile;
 const STORAGE_PATH = path.join(__dirname, "storage", "students.json");
 
 class StudentDao {
+    constructor(storagePath) {
+        this.studentStoragePath = storagePath ? storagePath : STORAGE_PATH;
+    }
+    
+    async getStudent(id) {
+        let studentList = await this._loadAllStudents();
+        const result = studentList.find((b) => b.id === id);
+        return result;
+    }
 
+    async listStudents() {
+        let studentList = await this._loadAllStudents();
+        return studentList;
+    }
 
+    async _loadAllStudents() {
+        let studentList;
+        try {
+            studentList = JSON.parse(await rf(this._getStorageLocation()));
+        } catch (e) {
+            if (e.code === "ENOENT") {
+            console.info("No storage found, initializing new one...");
+            studentList = [];
+            } else {
+                throw new Error(
+                    "Unable to read from storage. Wrong data format. " +
+                        this._getStorageLocation()
+                );
+            }
+        }
+        return studentList;
+    }
+        
+    _getStorageLocation() {
+        return this.studentStoragePath;
+    }
 }
 
 module.exports = StudentDao;
--- server/dao/subject-dao.js
+++ server/dao/subject-dao.js
@@ -10,8 +10,53 @@ const wf = fs.promises.writeFile;
 const STORAGE_PATH = path.join(__dirname, "storage", "subjects.json");
 
 class SubjectDao {
+    constructor(storagePath) {
+        this.subjectStoragePath = storagePath ? storagePath : STORAGE_PATH;
+    }
 
+    async createSubject(subject) {
+        let subjectList = await this._loadAllSubjects();
+        subject.id = crypto.randomBytes(8).toString("hex");
+        subjectList.push(subject);
+        await wf(
+            this._getStorageLocation(),
+            JSON.stringify(subjectList, null, 2)
+        );
+        return recipe;
+    }
 
+    async getSubject(id) {
+        let subjectList = await this._loadAllSubjects();
+        const result = subjectList.find((b) => b.id === id);
+        return result;
+    }
+
+    async listSubject() {
+        let subjectList = await this._loadAllSubjects();
+        return subjectList;
+    }
+
+    async _loadAllSubjects() {
+        let subjectList;
+        try {
+            subjectList = JSON.parse(await rf(this._getStorageLocation()));
+        } catch (e) {
+            if (e.code === "ENOENT") {
+            console.info("No storage found, initializing new one...");
+            subjectList = [];
+            } else {
+                throw new Error(
+                    "Unable to read from storage. Wrong data format. " +
+                        this._getStorageLocation()
+                );
+            }
+        }
+        return subjectList;
+    }
+        
+    _getStorageLocation() {
+        return this.subjectStoragePath;
+    }
 }
-
+    
 module.exports = SubjectDao;
