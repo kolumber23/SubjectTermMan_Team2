c7ac31dcd543e451b0cec5fcce0c3901dc6bcc1a
Marek Urban <marek.urban@unicorn.com>
2024-04-23 21:52:39 +0200

    login env param co jwt potřebuje

--- PM/src/app.js
+++ PM/src/app.js
@@ -4,9 +4,17 @@ const subjectRoutes = require('./routes/subjectRoutes');
 const subjectTermRoutes = require('./routes/subjectTermRoutes');
 const studentRoutes = require('./routes/studentRoutes');
 const frontendProxy = require('./routes/frontendProxy');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3011;
+
+
+if(!process.env.JWT_SECRET)
+{
+  process.env.JWT_SECRET = "dave"
+}
+
 
 app.use(bodyParser.json());
 app.use(express.json());
@@ -17,6 +25,7 @@ app.use('/api/subject', subjectRoutes);
 app.use('/api/subjectTerm', subjectTermRoutes);
 
 app.use('/api/student', studentRoutes);
+app.use('/api', authRoutes);
 app.use('/', frontendProxy);
 
 app.listen(PORT, () => {
